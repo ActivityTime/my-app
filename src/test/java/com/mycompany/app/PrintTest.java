5521d27408633d609e235a88b507fb0620ecc51d
ACTIM <actim@foxmail.com>
2025-04-17 13:54:25 +0800

    jjjj

--- src/test/java/com/mycompany/app/PrintTest.java
+++ src/test/java/com/mycompany/app/PrintTest.java
@@ -6,7 +6,7 @@ public class PrintTest {
 
     @Test
     public void testPrint() {
-        String expectedOutput = "Hello, World!==Hello, World!=java.version=17.0.2";
+        String expectedOutput = "Hello, World!==Hello,999, World!=java.version=17.0.2";
         System.out.println(expectedOutput);
     }
 }
